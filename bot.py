d82addab224fd5b4639435b52ea9fda15e6ddd70
Tayden Flitcroft <taydengoodeill@gmail.com>
2024-01-26 17:46:58 -0600

    adding descriptions to commands

--- bot.py
+++ bot.py
@@ -8,17 +8,20 @@ load_dotenv()
 
 bot = commands.Bot('!pal ', intents=discord.Intents.all())
 
-@bot.command()
-async def view_settings(ctx: commands.context):
+@bot.command(help="View the current game settings.")
+async def view_settings(ctx: commands.Context):
     settings = GameSettings()
     settings_messages = settings.format_settings_for_discord()
     for msg in settings_messages:
         await ctx.send(msg)
 
-@bot.command()
-async def location(ctx: commands.context):
+@bot.command(help="Get the IP address of the server. (Admin Only)")
+@commands.has_role('Admin')
+async def location(ctx: commands.Context):
     settings = GameSettings().settings
-    await ctx.send('Server IP is: ' + settings['PublicIP'] + ':' + settings['PublicPort'])
+    await ctx.author.send(f'The Palworld server IP address is: {settings["PublicIP"]}:{settings["PublicPort"]}')
+    await ctx.send(f"{ctx.author.mention}, I have sent you a direct message with the server information. Please share sparingly! 😊")
+
 
 bot.run(environ['TOKEN'])
 
