fbc32587c2ce0420af8b09030d2a3466a2982029
Tayden Flitcroft <taydengoodeill@gmail.com>
2024-01-27 02:55:17 -0600

    fix async handler for update method

--- cogs/control_bot.py
+++ cogs/control_bot.py
@@ -8,6 +8,6 @@ class ControlBot(commands.Cog, name="Control Bot"):
 
     @commands.command(name='updatebot', help='Temporarily shuts down the bot to update it. ("Palworld Pals" role required)')
     @commands.has_role("Palworld Pals")
-    def update_bot(self):
+    async def update_bot(self):
         self.ssh.send_command(open('helpers/scripts/update_bot.sh').read())
         return
