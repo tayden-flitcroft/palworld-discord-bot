1e39a0e903851237799ee39b06f0bf70890f8f52
Tayden Flitcroft <taydengoodeill@gmail.com>
2024-01-29 22:52:53 -0600

    updating installer process in dockerfile

--- cogs/mods.py
+++ cogs/mods.py
@@ -14,7 +14,7 @@ class Mods(commands.Cog, name="Mods"):
         if ctx.invoked_subcommand is None:
             await ctx.send('Mods command group. Use `mods <subcommand>`.')
 
-    @mods.command(name='download', help='Downloads all mods required for the server.')
-    async def download(self, ctx):
-        await ctx.send('Executable has been generated. Download and install for all mods currently used on the server.')
+    @mods.command(name='installer', help='Downloads mods installer.')
+    async def installer(self, ctx):
+        await ctx.send('Executable has been generated. Download and run to install all mods currently used on the server.')
         await ctx.send(file=discord.File('dist/Palworld Mod Installer'))
